7dff93543d427791679b7b0740dac60332b3eff4
ggros <gui.gros@gmail.com>
2020-01-19 12:03:34 +0100

    Update partner_gogocartojs/__manifest__.py
    add odoo version prefix to the version
    
    Co-Authored-By: Nicolas JEUDY <njeudy@myceliandre.fr>

--- partner_gogocartojs/__manifest__.py
+++ partner_gogocartojs/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Website',
-    'version': '0.1',
+    'version': '12.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
