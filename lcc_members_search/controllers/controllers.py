d334d6b14b11c0b0192c996982f205f358d6c7b9
root <root@lokavaluto.lokavaluto.odoo>
2020-03-23 16:46:41 +0100

    add notes files

--- lcc_members_search/controllers/controllers.py
+++ lcc_members_search/controllers/controllers.py
@@ -5,29 +5,32 @@ from odoo import http
 class LccMembersSearch(http.Controller):
    @http.route('/lcc_members_search/lcc_members_search/', auth='public')
    def index(self, **kw):
-      #return "Hello, world1"
-      return http.request.render('lcc_members_search.index', {
+      return http.request.render('lcc_members_search.searchform', {
          'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
-      })
-
-   @http.route('/lcc_members_search/lcc_members_search/objects/', auth='public')
-   def list(self, **kw):
-      return http.request.render('lcc_members_search.listing', {
-         'root': '/lcc_members_search/lcc_members_search',
-         'objects': http.request.env['lcc_members_search.lcc_members_search'].search([]),
       })
 
    @http.route('/lcc_members_search/lcc_members_search/search/', auth='public',website=True, csrf=False)
    def search(self, **kw):
       memberid = kw['member_id']
-      results = http.request.env['res.partner'].search([('id', '==', 1)])
-      return "Search page,{} ".format(kw['member_id'])
-      #return http.request.render('lcc_members_search.listing', {
-      #   'root': '/lcc_members_search/lcc_members_search',
-      #  'objects': http.request.env['lcc_members_search.lcc_members_search'].search([]),
-      #})
-      # retrieve the membre id to search 
+      int_memberid = int(memberid)
+      partners = http.request.env['res.partner'].search([]),
+      partners_is_empty = False
+      #if partners.count() <=0 partners_is_empty = True
 
+      #results = self.env['res.partner'].search(['is_company', '=', True])
+      #return "Search page,{} ".format(results)
+      return http.request.render('lcc_members_search.listing', {
+         'root': '/lcc_members_search/lcc_members_search',
+         'partners': http.request.env['res.partner'].search([]),
+         'partners_is_empty': partners_is_empty
+      })
+
+   #@http.route('/lcc_members_search/lcc_members_search/objects/', auth='public')
+   #def list(self, **kw):
+   #   return http.request.render('lcc_members_search.listing', {
+   #      'root': '/lcc_members_search/lcc_members_search',
+   #      'objects': http.request.env['lcc_members_search.lcc_members_search'].search([]),
+   #   })
 
 #     @http.route('/lcc_members_search/lcc_members_search/objects/<model("lcc_members_search.lcc_members_search"):obj>/', auth='public')
 #     def object(self, obj, **kw):
