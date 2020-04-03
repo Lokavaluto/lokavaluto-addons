185976878c1e44562442f87de20e3b29702feedf
root <root@lokavaluto.lokavaluto.odoo>
2020-04-03 23:03:59 +0200

    Updating Domain and algorithm

--- lcc_members_search/controllers/controllers.py
+++ lcc_members_search/controllers/controllers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import http
-
+#@RBO
 
 class LccMembersSearch(http.Controller):
    @http.route('/lcc_members_search/lcc_members_search/', auth='public')
@@ -14,11 +14,13 @@ class LccMembersSearch(http.Controller):
       firstname   = kw['firstname']
       zipcode     = kw['zipcode']
       email       = kw['email']
+
+      #init all variables here
       allisempty  = False
       partners_is_empty = False
-
+      
+      #test for field content
       if len(memberid)<=0 and len(lastname)<=0 and len(firstname)<=0 and len(zipcode)<=0 and len(email)<=0:
-         print('all empty')
          allisempty = True
          partners_is_emptyfull = False
          return http.request.render('lcc_members_search.listing', {
@@ -32,55 +34,40 @@ class LccMembersSearch(http.Controller):
             int_memberid = 0
          #N°adhérent -> char , field -> ref
          #ref ou (codepostal et nom et prénom et email)
+         #replace id field with ref one and memberid string variable
          searchfilter =[('id','=',int_memberid), 
-                        '|',('name','ilike',lastname +'%'),
+                        '|',('lastname','ilike',lastname +'%'),
+                           ('firstname','ilike',firstname +'%'),
                            ('email','=',email),
                            ('zip','=',zipcode)
                      ]
          partners = http.request.env['res.partner'].search(searchfilter)
          partnerscount = http.request.env['res.partner'].search_count(searchfilter)
          
-         if partnerscount <= 1:
+         if partnerscount == 1:
             searchfilter =[('id','=',int_memberid), 
-                        '|',('name','ilike',lastname +'%'),
-                           ('email','=',email),
-                           ('zip','=',zipcode),
-                           ('membership_state', 'in', ['invoiced','paid','free'])
+                           '|',('lastname','ilike',lastname +'%'),
+                              ('firstname','ilike',firstname +'%'),
+                              ('email','=',email),
+                              ('zip','=',zipcode),
+                              ('membership_state', 'in', ['invoiced','paid','free'])
                      ]
+            
             member_found = True
             partners = http.request.env['res.partner'].search(searchfilter)
             partnerscount = http.request.env['res.partner'].search_count(searchfilter)
             return http.request.render('lcc_members_search.listing', {
-               'root': '/lcc_members_search/lcc_members_search',
-               'partners': partners,
-               'partners_is_emptyfull': partners_is_emptyfull,
-               'criterias': {'int_memberid':int_memberid,'lastname':lastname,'zipcode':zipcode,'email':email},
-               'searchfilter': searchfilter,
-               'partnerscount':partnerscount,
+               #'partners': partners,
+               'partners_is_one': True,
+               #'searchfilter': searchfilter,
+               #'partnerscount':partnerscount,
                'member_found':member_found,
-               'allisempty':allisempty
             })
-         else
-            partners_is_emptyfull = True #si 
+         else:
+            partners_is_emptyfull = True 
             return http.request.render('lcc_members_search.listing', {
-               'root': '/lcc_members_search/lcc_members_search',
                'partners': partners,
-               'partners_is_emptyfull': partners_is_emptyfull,
-               'criterias': {'int_memberid':int_memberid,'lastname':lastname,'zipcode':zipcode,'email':email},
-               'searchfilter': searchfilter,
-               'partnerscount':partnerscount,
-               'allisempty':allisempty
+               'partners_is_emptyfull': partners_is_emptyfull
+               #'searchfilter': searchfilter,
+               #'partnerscount':partnerscount,
             })
-
-   #@http.route('/lcc_members_search/lcc_members_search/objects/', auth='public')
-   #def list(self, **kw):
-   #   return http.request.render('lcc_members_search.listing', {
-   #      'root': '/lcc_members_search/lcc_members_search',
-   #      'objects': http.request.env['lcc_members_search.lcc_members_search'].search([]),
-   #   })
-
-#     @http.route('/lcc_members_search/lcc_members_search/objects/<model("lcc_members_search.lcc_members_search"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('lcc_members_search.object', {
-#             'object': obj
-#         })
