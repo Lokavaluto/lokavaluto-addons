5db9efe430cc00232c85a774e462d709dbaf459f
Stéphan Sainléger <contact@stephansainleger.fr>
2020-10-09 15:47:21 +0200

    Creation of Lokavaluto mobile app connection module

---
+++ lcc_lokavaluto_app_connection/controllers/main.py
@@ -0,0 +1,32 @@
+from odoo import http
+
+class MobileApplicationJson(http.Controller):
+
+
+    @http.route('/web/get_application_taxonomy',methods=['POST'], type='json', csrf=False, auth="public")
+    def get_app_map_taxonomy(self, **kwargs):
+        data = {}
+        categories = self.env['res.partner.industry'].search([('in_gogocarto','=',True)])
+        for category in categories:
+            data.append({
+                id: category.id;
+                name: category.name;
+                color: category.color;
+                # later:
+                # icon: category.icon  --> field icon to create in model category
+            })
+        return Response(json.dumps(data),content_type='application/json;charset=utf-8',status=200)
+
+
+    @http.route('/web/get_application_elements',methods=['POST'], type='json', csrf=False, auth="public")
+    def get_app_pro_contacts_on_area(self, **kwargs):
+        data = {}
+        all_partner = self.env['res.partner']
+        partners = all_partner.search(all_partner._get_mobile_app_pro_domain())
+        for partner in partners:
+            data.append({
+                id: partner.id;
+                name: partner.name;            
+            })
+        return Response(json.dumps(data),content_type='application/json;charset=utf-8',status=200)
+
