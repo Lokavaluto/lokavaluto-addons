f6a3a543ea25b40b76c26adcade5530473ce38f7
MigayrouChloe <ubucmu@DESKTOP-0NJKHAP.localdomain>
2020-10-10 15:59:57 +0200

    Update controller that retrieves partner for the app

--- lcc_lokavaluto_app_connection/models/res_partner.py
+++ lcc_lokavaluto_app_connection/models/res_partner.py
@@ -6,8 +6,11 @@ class ResPartner(models.Model):
 
     in_mobile_app = fields.Boolean('In the mobile map')
     
-    _get_mobile_app_pro_domain(self)
-        return [('in_mobile_app','=',True),('company_type','=', 'company')]
+    def _get_mobile_app_pro_domain(self, **kwargs):
+        return [('in_mobile_app','=',True),
+                ('company_type','=', 'company'),
+                ("partner_longitude">kwargs.get("minLon") and "partner_longitude"<kwargs.get("maxLon")),
+                ("partner_latitude">kwargs.get("minLat") and "partner_latitude"<kwargs.get("maxLat"))]
 
-    _get_mobile_app_contact_domain(self)
-        return [('in_mobile_app','=',True))]
+    def _get_mobile_app_contact_domain(self):
+        return [('in_mobile_app','=',True)]
