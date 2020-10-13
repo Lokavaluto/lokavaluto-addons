7165ae2f5e3fa01a1d09b8021dcd3922748ac84f
Stéphan Sainléger <contact@stephansainleger.fr>
2020-10-13 18:02:41 +0200

    Start development to limit access of exchange counters to their own data

--- lcc_exchange_counters/models/__init__.py
+++ lcc_exchange_counters/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import exchange_counters_config
+from . import exchange_counters_inventory
--- lcc_exchange_counters/models/exchange_counters_config.py
+++ lcc_exchange_counters/models/exchange_counters_config.py
@@ -6,6 +6,16 @@ class exchange_counters_config(models.Model):
     _description = "MLCC Counter Config"
     _inherits = {'pos.config': 'pos_config_id'}
 
+    counter_referent_id = fields.Many2one(
+        'res.users',
+        string=_("Counter referent"),
+        required=True,
+        translate=False,
+        readonly=False,
+        default=lambda 
+        self: self.env.user
+    )
+
     def _get_group_pos_manager(self):
         return self.env.ref('lcc_exchange_counters.group_exchange_counters_manager')
 
