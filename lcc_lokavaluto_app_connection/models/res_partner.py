aad9171261b4cda5770713072bac1aa7778b127e
MigayrouChloe <ubucmu@DESKTOP-0NJKHAP.localdomain>
2020-10-15 13:28:20 +0200

    Update uses of parameter

--- lcc_lokavaluto_app_connection/models/res_partner.py
+++ lcc_lokavaluto_app_connection/models/res_partner.py
@@ -4,13 +4,15 @@ class ResPartner(models.Model):
     """ Inherits partner and adds Tasks information in the partner form """
     _inherit = 'res.partner'
 
-    in_mobile_app = fields.Boolean('In the mobile map')
+    in_mobile_app = fields.Boolean('In the mobile map', default = True)
     
     def _get_mobile_app_pro_domain(self, **kwargs):
         return [('in_mobile_app','=',True),
-                ('company_type','=', 'company'),
-                ("partner_longitude">kwargs.get("minLon") and "partner_longitude"<kwargs.get("maxLon")),
-                ("partner_latitude">kwargs.get("minLat") and "partner_latitude"<kwargs.get("maxLat"))]
+                ('is_company','=', True),
+                ('partner_longitude', '>', kwargs.get("minLon")),
+                ('partner_longitude', '<', kwargs.get("maxLon")),
+                ('partner_latitude','>', kwargs.get("minLat")),
+                ('partner_latitude', '<', kwargs.get("maxLat"))]
 
     def _get_mobile_app_contact_domain(self):
         return [('in_mobile_app','=',True)]
