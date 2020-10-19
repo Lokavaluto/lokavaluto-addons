e0f6d855f7da273e7023f497e0c9dfb3712d0271
default <default@ns3033602>
2020-10-19 13:09:25 +0000

    [FIX] bump version

--- lcc_lokavaluto_app_connection/__manifest__.py
+++ lcc_lokavaluto_app_connection/__manifest__.py
@@ -73,7 +73,7 @@ mission is to support the collaborative development of Odoo features and ecosyst
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Website',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
