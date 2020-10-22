50336277061c84ba2f7f99db717de6a278549ca6
MigayrouChloe <ubucmu@DESKTOP-0NJKHAP.localdomain>
2020-10-22 13:50:54 +0200

    Update JSON Serializaition

--- lcc_lokavaluto_app_connection/controllers/main.py
+++ lcc_lokavaluto_app_connection/controllers/main.py
@@ -32,37 +32,5 @@ class MobileApplicationJson(http.Controller):
         bounding_box = request.jsonrequest['bounding_box']
         partners = all_partner.search(all_partner._get_mobile_app_pro_domain(**bounding_box))
         for partner in partners:
-            data.append({
-                "autocompleteLabel": partner._get_autocompleteLabel(), 
-                "city":partner.city, 
-                "phones": [partner.phone, partner.mobile],
-                "adherent": partner._is_partner_adherent(),
-                #TODO "mainICC":,
-                #TODO "keywords":[],
-                "name": partner.name,
-                "addresse":{
-                    #TODO "id":,
-                    "street1":partner.street,
-                    "street2":partner.street2,
-                    "latitude":partner.partner_latitude,
-                    "longitude":partner.partner_longitude,
-                    "zipCity":{
-                        #TODO "id":,
-                        "zipCode":partner.zip,
-                        "city":partner.city,
-                        "name": partner._get_zipCityName()
-                    },
-                },
-                "excerpt": partner.website_short_description,
-                "description": partner.website_description,
-                #TODO "image": partner.image, # return public image URL 
-                #TODO firstLogin: true,
-                "url": partner.website_url, 
-                "id": partner.id,
-                #TODO "username": "Pr0177",
-                "email": partner.email
-                #TODO "lastLogin": null,
-                #TODO "roles": ["ROLE_PRO", "ROLE_USER"],
-                #TODO "enabled": false      
-            }) 
-        return json.dumps(data)
+            data.append(partner.app_serialization())
+        return data
