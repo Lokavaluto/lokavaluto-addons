2141bc9fe1e2207a33be75117a72f4c72e1bc314
MigayrouChloe <ubucmu@DESKTOP-0NJKHAP.localdomain>
2020-10-22 13:50:54 +0200

    Update JSON Serializaition

--- lcc_lokavaluto_app_connection/models/res_partner.py
+++ lcc_lokavaluto_app_connection/models/res_partner.py
@@ -44,6 +44,39 @@ class ResPartner(models.Model):
 
     def in_mobile_app_button(self):
         """ Inverse the value of the field ``in_mobile_app`` for the current instance. """     
-        self.in_mobile_app = not self.in_mobile_app     
-       
+        self.in_mobile_app = not self.in_mobile_app   
+
+#region Public method for JSON Serialization
+    def app_serialization(self):
+        element = {}
+        self.__add_simple_node(element, "id")
+        self.__add_simple_node(element, "name")
+        self.__add_computed_node(element, "autoCompleteLabel", self._get_autocompleteLabel)
+        self.__add_computed_node(element,"adherent", self._is_partner_adherent)
+        # self.__add_computed_node(element,"image", self.__getImage)
+        # TODO : mainICC
+        # TODO : keywords
+        self.__add_simple_node(element, "email")
+        self.__add_simple_node(element, "website")
+        self.__add_simple_node(element, "website_description")
+        self.__add_simple_node(element, "website_short_description")
+        self.__add_nested_node(element, "address", "street", "street2","zip", "city")
+        self.__add_nested_node(element, "coords", "partner_latitude", "partner_longitude")
+        self.__add_nested_node(element, "phones", "phone", "mobile")
+        return element
+#endregion
+        
+#region Private method to JSON Serialization
+    def __add_simple_node(self, element, fieldName): 
+        if getattr(self, fieldName):
+            element[fieldName] = self[fieldName]
     
+    def __add_computed_node(self, element, fieldLabel, specificMethod):
+        element[fieldLabel] = specificMethod()
+
+    def __add_nested_node(self, element, nestedName, *args):
+        nest = {}
+        for arg in args:
+            self.__add_simple_node(nest, arg)
+        element[nestedName] = nest
+#endregion
