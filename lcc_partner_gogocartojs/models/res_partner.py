cc170c4f85cf10467d16f820c74f87740e169e2c
Stéphan Sainléger <contact@stephansainleger.fr>
2020-10-23 17:38:35 +0200

    Update JSON Serialization for Gogocarto module

--- lcc_partner_gogocartojs/models/res_partner.py
+++ lcc_partner_gogocartojs/models/res_partner.py
@@ -10,8 +10,6 @@ class ResPartner(models.Model):
         return [('in_gogocarto','=',True),('is_company','=', True),('membership_state','in',['paid', 'free'])]
 
 
-        
-
     def _get_exchange_counter_label(self):
         if self.currency_exchange_office:
             return 'Exchange counter'
@@ -23,3 +21,36 @@ class ResPartner(models.Model):
             return 'Itinerant'
         else:
             return ''
+
+    #region Public method for JSON Serialization
+    def app_serialization(self):
+        element = {}
+        self.__add_simple_node(element, "id")
+        self.__add_simple_node(element, "team_id.name")
+        self.__add_simple_node(element, "name")
+        self.__add_nested_node(element, "address", "street", "street2","zip", "city")
+        self.__add_nested_node(element, "coords", "partner_latitude", "partner_longitude")
+        self.__add_nested_node(element, "contact_info", "phone", "mobile", "email", "website")
+        self.__add_simple_node(element, "convention_signature_date")
+        self.__add_nested_node(element, "activities", "industry_id.name", "detailed_activity", "member_comment", "opening_time","legal_activity_code")
+        self.__add_computed_node(element,"exchange_counter", self._get_exchange_counter_label)
+        self.__add_computed_node(element,"itinerant", self._get_itinerant_label)
+        self.__add_simple_node(element, "keywords")
+        self.__add_simple_node(element,"reasons_choosing_mlc")
+        return element
+    #endregion
+        
+    #region Private method to JSON Serialization
+    def __add_simple_node(self, element, fieldName): 
+        if getattr(self, fieldName):
+            element[fieldName] = self[fieldName]
+    
+    def __add_computed_node(self, element, fieldLabel, specificMethod):
+        element[fieldLabel] = specificMethod()
+
+    def __add_nested_node(self, element, nestedName, *args):
+        nest = {}
+        for arg in args:
+            self.__add_simple_node(nest, arg)
+        element[nestedName] = nest
+    #endregion
