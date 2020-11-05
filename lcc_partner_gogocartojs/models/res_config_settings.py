d9264b43e20dce9762c65888d28655f64fc61758
MigayrouChloe <ubucmu@DESKTOP-0NJKHAP.localdomain>
2020-11-05 14:37:42 +0100

    Save partner fields for gogocarto

--- lcc_partner_gogocartojs/models/res_config_settings.py
+++ lcc_partner_gogocartojs/models/res_config_settings.py
@@ -9,7 +9,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     gogocarto_map_url = fields.Char(readonly=False)
-    export_gogocarto_fields = fields.Many2many('ir.model.fields',
+    export_gogocarto_fields = fields.Many2many('ir.model.fields', 'export_field_rel', 'export_id', 'fields_id',
                                                domain="[""('model_id', '=', 'res.partner')""]",
                                                readonly=False)
 
@@ -21,10 +21,11 @@ class ResConfigSettings(models.TransientModel):
         export_gogocarto_fields = ICPSudo.get_param('lcc_partner_gogocartojs.export_gogocarto_fields')    
         res.update(
             gogocarto_map_url=gogocarto_map_url,
-            # export_gogocarto_fields=(6,0,literal_eval(export_gogocarto_fields))
+            export_gogocarto_fields= [(6, 0, literal_eval(export_gogocarto_fields))]
         )
         return res
 
+    @api.multi
     def set_values(self):
         res = super(ResConfigSettings, self).set_values()
         ICPSudo = self.env['ir.config_parameter']
