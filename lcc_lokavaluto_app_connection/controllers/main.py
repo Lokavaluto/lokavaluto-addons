83675427ebb66a2fdcf252ef9716352b3e391540
default <default@ns3033602>
2021-02-15 18:18:53 +0000

    [UPD] add openapi squeleton a prepare auth_api_key

--- lcc_lokavaluto_app_connection/controllers/main.py
+++ lcc_lokavaluto_app_connection/controllers/main.py
@@ -1,18 +1,16 @@
 import json, logging
 
-from odoo import api, fields, http, models
+from odoo import http
 from odoo.http import Response, request
-
-
-
 _logger = logging.getLogger(__name__)
+
 
 class MobileApplicationJson(http.Controller):
 
-    @http.route('/web/get_application_taxonomy',methods=['POST'], type='json', csrf=False, auth="public")
+    @http.route('/web/get_application_taxonomy', methods=['POST'], type='json', csrf=False, auth="public")
     def get_app_map_taxonomy(self, **kwargs):
         data = []
-        categories = request.env['res.partner.industry'].search([('in_gogocarto','=',True)])
+        categories = request.env['res.partner.industry'].search([('in_gogocarto', '=', True)])
         for category in categories:
             data.append({
                 "id": category.id,
@@ -21,13 +19,11 @@ class MobileApplicationJson(http.Controller):
                 # "later":
                 # "icon": category.icon  --> field icon to create in model category
             })
-        return Response(json.dumps(data),content_type='application/json;charset=utf-8',status=200)
+        return Response(json.dumps(data), content_type='application/json;charset=utf-8', status=200)
 
-
-    @http.route('/web/get_application_elements',methods=['POST'], type='json', csrf=False, auth="public", website=True)
+    @http.route('/web/get_application_elements', methods=['POST'], type='json', csrf=False, auth="public", website=True)
     def get_app_pro_contacts_on_area(self, **kwargs):
         data = []
-        #TODO : remove .sudo()
         all_partner = request.env['res.partner'].sudo()
         bounding_box = request.jsonrequest['bounding_box']
         partners = all_partner.search(all_partner._get_mobile_app_pro_domain(**bounding_box))
