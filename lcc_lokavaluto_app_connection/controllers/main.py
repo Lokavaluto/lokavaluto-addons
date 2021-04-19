689ee87afda2715a2950ccc9185fb2c8e13f929a
Gaspard "Sarp" Lonchampt <71872843+gaspard-lonchampt@users.noreply.github.com>
2021-04-19 13:37:25 +0200

    Update main.py
    Allow multiple origin for CORS policy.

--- lcc_lokavaluto_app_connection/controllers/main.py
+++ lcc_lokavaluto_app_connection/controllers/main.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 class MobileApplicationJson(http.Controller):
 
-    @http.route('/web/get_application_taxonomy', methods=['POST'], type='json', csrf=False, auth="public")
+    @http.route('/web/get_application_taxonomy', methods=['POST'], type='json', csrf=False, auth="public", cors='*')
     def get_app_map_taxonomy(self, **kwargs):
         data = []
         categories = request.env['res.partner.industry'].search([('in_gogocarto', '=', True)])
@@ -21,7 +21,7 @@ class MobileApplicationJson(http.Controller):
             })
         return Response(json.dumps(data), content_type='application/json;charset=utf-8', status=200)
 
-    @http.route('/web/get_application_elements', methods=['POST'], type='json', csrf=False, auth="public", website=True)
+    @http.route('/web/get_application_elements', methods=['POST'], type='json', csrf=False, auth="public", website=True, cors='*')
     def get_app_pro_contacts_on_area(self, **kwargs):
         data = []
         all_partner = request.env['res.partner'].sudo()
