5fe884c6f102b50b30fd4d973b92bca0374c33fd
default <default@vps790812>
2021-06-15 11:14:40 +0200

    [UPD] add filter on IDs on partner_industry API service

--- lcc_lokavaluto_app_connection/services/partner_industry_services.py
+++ lcc_lokavaluto_app_connection/services/partner_industry_services.py
@@ -15,27 +15,39 @@ class PartnerIndustryService(Component):
         Access to the Partner Industry services is allowed to everyone
     """
 
-    def get_all(self):
-        """
-        Get the complete list of partner indutries
-        """
-        all_industries = self.env['res.partner.industry'].sudo()
-        industries = all_industries.search([('active', '=', True)])
-        rows = []
-        res = {"count": len(industries), "rows": rows}
-        parser = self._get_partner_industry_parser()
-        rows = industries.jsonify(parser)
-        _logger.debug('#################### rows: %s' % rows)
-        res = {"count": len(industries), "rows": rows}
-        return res
-
+    def get(self, ids=[]):
+         """
+         Get the complete list of partner indutries
+         """
+         all_industries = self.env['res.partner.industry'].sudo()
+         if ids:
+             industries = all_industries.search([('active', '=', True),('id', 'in', ids)])
+         else:
+             industries = all_industries.search([('active', '=', True)])
+         rows = []
+         res = {"count": len(industries), "rows": rows}
+         parser = self._get_partner_industry_parser()
+         rows = industries.jsonify(parser)
+         _logger.debug('#################### rows: %s' % rows)
+         res = {"count": len(industries), "rows": rows}
+         return res
     
 
     # The following method are 'private' and should be never never NEVER call
     # from the controller.
 
     # Validator
-    def _validator_return_get_all(self):
+    def _validator_get(self):
+        return {"ids" : {
+                    "type": "list", 
+                    "schema": {"type": "integer"},
+                    "required": False,
+                    "nullable": True
+                },
+               }
+
+
+    def _validator_return_get(self):
         res = {
             "count": {"type": "integer", "required": True},
             "rows": {
