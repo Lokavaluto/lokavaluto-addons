12078b8a953c1abbdfeecdb023c7f0737b418b3b
default <default@dev.myceliandre.fr>
2021-06-25 07:45:10 +0000

    [FIX] typo

--- lcc_lokavaluto_app_connection/models/res_partner.py
+++ lcc_lokavaluto_app_connection/models/res_partner.py
@@ -34,8 +34,17 @@ class ResPartner(models.Model):
                     ('partner_latitude', '>', float(bounding_box.get('minLat', ''))),
                     ('partner_latitude', '<', float(bounding_box.get('maxLat', '')))]
 
-
     def in_mobile_app_button(self):
         """ Inverse the value of the field ``in_mobile_app``
             for the current instance. """
         self.in_mobile_app = not self.in_mobile_app
+
+    def _update_auth_data(self, password):
+        return {}
+    
+    def _validator_return_authenticate(self):
+        return {"response": {"type": "dict", "schema": {"uid": {"type": "integer"},
+                                                        "partner_id": {"type": "integer"},
+                                                        "status": {"type": "string", "required": True},
+                                                        "error": {"type": "string"},
+                                                        "api_token": {"type": "string",}}}}
