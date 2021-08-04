e7f59984d02184cb621906f86c9a1f85c29b4027
default <default@vps790812>
2021-08-04 21:38:56 +0200

    [UPD] Fusion of partner/get_by_url service and partner/get

--- lcc_lokavaluto_app_connection/datamodel/partner_info.py
+++ lcc_lokavaluto_app_connection/datamodel/partner_info.py
@@ -4,6 +4,14 @@ from odoo.addons.datamodel.core import Datamodel
 from odoo.addons.datamodel.fields import NestedModel
 
 
+class PartnerInfoGetParam(Datamodel):
+    _name = "partner.info.get.param"
+
+    id = fields.Integer(required=False, allow_none=True)
+    website_url = fields.String(required=False, allow_none=True)
+    backend_keys = fields.List(fields.String())
+
+
 class PartnerSearchInfo(Datamodel):
     _name = "partner.search.info"
 
@@ -13,3 +21,4 @@ class PartnerSearchInfo(Datamodel):
     offset = fields.Integer(required=False, allow_none=True)
     limit = fields.Integer(required=False, allow_none=True)
     website_url = fields.String(required=False, allow_none=True)
+
