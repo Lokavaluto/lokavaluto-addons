1dfd4cd78e573bb7e220fb8a22ddb4f8eaebb217
Valentin Lab <valentin.lab@kalysto.org>
2021-12-01 21:10:57 +0100

    [UPD] rename ``comchain_server_url`` to ``comchain_currency_name``
    There are no server url for comchain. The new currency name is to give
    a default currency name for new comers. Even if nothing prevents a
    register on another currency.
    
    Signed-off-by: Valentin Lab <valentin.lab@kalysto.org>

--- lcc_comchain_base/models/res_config_settings.py
+++ lcc_comchain_base/models/res_config_settings.py
@@ -4,14 +4,8 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    comchain_server_url = fields.Char(
-        related='company_id.comchain_server_url',
-        readonly=False,
-        string='Url for comchain serveur'
-    )
-
     # Temporary way to handle only one comchain currency per odoo
-    currency_name = fields.Char(
+    comchain_currency_name = fields.Char(
         related='company_id.comchain_currency_name',
         readonly=False,
         string='Currency Name'
