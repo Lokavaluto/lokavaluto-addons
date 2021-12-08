198d541865bbe9c28fe2311c4739c640c7474698
Nicolas JEUDY <nicolas.jeudy@myceliandre.fr>
2021-12-08 11:35:14 +0100

    [FIX] typo and attachment option on binary fields.

--- lcc_members/models/res_partner.py
+++ lcc_members/models/res_partner.py
@@ -15,120 +15,89 @@ class res_partner(models.Model):
         string=_("Why did I choose local currency"),
         required=False,
         translate=True,
-        readonly=False
+        readonly=False,
     )
     phone_pro = fields.Char(
-        string=_("Professional phone"),
-        required=False,
-        translate=False,
-        readonly=False
+        string=_("Professional phone"), required=False, translate=False, readonly=False
     )
     member_comment = fields.Text(
-        string=_("Member comment"),
-        required=False,
-        translate=False,
-        readonly=False
-    )    
+        string=_("Member comment"), required=False, translate=False, readonly=False
+    )
     member_type_id = fields.Many2one(
-        'member.type',
+        "member.type",
         string=_("Member's type"),
         required=False,
         translate=False,
-        readonly=False
+        readonly=False,
     )
     convention_signature_date = fields.Date(
         string=_("convention signature date"),
         required=False,
         translate=False,
-        readonly=False
+        readonly=False,
     )
     to_renew = fields.Boolean(
-        string=_("To renew"),
-        required=False,
-        translate=False,
-        readonly=False
+        string=_("To renew"), required=False, translate=False, readonly=False
     )
     legal_activity_code = fields.Char(
         string=_("legal activity code (ex. APE in France)"),
         required=False,
         translate=False,
-        readonly=False
+        readonly=False,
     )
     keywords = fields.Text(
-        string=_("keywords"),
-        required=False,
-        translate=True,
-        readonly=False
+        string=_("keywords"), required=False, translate=True, readonly=False
     )
     opening_time = fields.Text(
-        string=_("Opening Time"),
-        required=False,
-        translate=True,
-        readonly=False
+        string=_("Opening Time"), required=False, translate=True, readonly=False
     )
     itinerant = fields.Boolean(
-        string=_("Itinerant"),
-        required=False,
-        translate=False,
-        readonly=False
+        string=_("Itinerant"), required=False, translate=False, readonly=False
     )
     detailed_activity = fields.Char(
-        string=_("Detailed activity"),
-        required=False,
-        translate=True,
-        readonly=False
+        string=_("Detailed activity"), required=False, translate=True, readonly=False
     )
     is_volunteer = fields.Boolean(
-        string=_("Is volunteer"),
-        required=False,
-        translate=False,
-        readonly=False
+        string=_("Is volunteer"), required=False, translate=False, readonly=False
     )
-    private_comment = fields.Text( ## TODO v4 : TO BE DELETED
-        string=_("Private comment"),
-        required=False,
-        translate=False,
-        readonly=False
+    private_comment = fields.Text(  ## TODO v4 : TO BE DELETED
+        string=_("Private comment"), required=False, translate=False, readonly=False
     )
-    convention_specific_agreement = fields.Text( ## TODO v4 : TO BE DELETED
+    convention_specific_agreement = fields.Text(  ## TODO v4 : TO BE DELETED
         string=_("Convention specific agreement"),
         required=False,
         translate=False,
-        readonly=False
+        readonly=False,
     )
     convention_agreement = fields.Binary(
-        string=_('Convention agreement'),
+        string=_("Convention agreement"),
+        attachment=True,
         required=False,
     )
     currency_exchange_office = fields.Boolean(
         string=_("Currency exchange office"),
         required=False,
         translate=False,
-        readonly=False
+        readonly=False,
     )
     discount = fields.Html(
         string="Discount",
         translate="True",
     )
-    partner_image_ids = fields.One2many( ## TODO v4 : TO BE DELETED
-        'partner.image',
-        'partner_id',
-        string='Images'
+    partner_image_ids = fields.One2many(  ## TODO v4 : TO BE DELETED
+        "partner.image", "partner_id", string="Images"
     )
     accept_coupons = fields.Boolean(
-        string=_("Accept coupons"),
-        required=False,
-        translate=False,
-        readonly=False
+        string=_("Accept coupons"), required=False, translate=False, readonly=False
     )
     accept_digital_currency = fields.Boolean(
         string=_("Accept digital currency"),
         required=False,
         translate=False,
-        readonly=False
+        readonly=False,
     )
     partner_profile = fields.Many2one(
-        'partner.profile',
+        "partner.profile",
         string=_("Partner profile"),
         required=False,
         translate=False,
@@ -137,29 +106,40 @@ class res_partner(models.Model):
 
     @api.model
     def create(self, vals):
-        """ When creating, if partner_profile is not defined by a previous process, the defaut value is Main"""
-        modified_self = self._basecontact_check_context('create')
-        if not vals.get('partner_profile') and not vals.get('contact_id'):
-            profile = self.env.ref('lcc_members.parter_profile_main').read()[0]
-            vals['partner_profile'] = profile['id']
+        """When creating, if partner_profile is not defined by a previous process, the defaut value is Main"""
+        modified_self = self._basecontact_check_context("create")
+        if not vals.get("partner_profile") and not vals.get("contact_id"):
+            profile = self.env.ref("lcc_members.parter_profile_main").read()[0]
+            vals["partner_profile"] = profile["id"]
         return super(res_partner, modified_self).create(vals)
 
-
-    @api.onchange('firstname', 'lastname', 'is_company')
+    @api.onchange("firstname", "lastname", "is_company")
     def onchange_upper_name(self):
-        if (not self.is_company) and self.lastname and self.lastname != self.lastname.upper():
+        if (
+            (not self.is_company)
+            and self.lastname
+            and self.lastname != self.lastname.upper()
+        ):
             self.lastname = self.lastname.upper()
-        if (not self.is_company) and self.firstname and self.firstname != self.firstname.capitalize():
+        if (
+            (not self.is_company)
+            and self.firstname
+            and self.firstname != self.firstname.capitalize()
+        ):
             self.firstname = self.firstname.capitalize()
 
     @api.model
     def search_position_partners(self, profile):
         if profile:
-            position_partners = self.env['res.partner'].search([('contact_id', '=', self.id),('partner_profile', '=', profile)])
+            position_partners = self.env["res.partner"].search(
+                [("contact_id", "=", self.id), ("partner_profile", "=", profile)]
+            )
         else:
-            position_partners = self.env['res.partner'].search([('contact_id', '=', self.id)])
+            position_partners = self.env["res.partner"].search(
+                [("contact_id", "=", self.id)]
+            )
         return position_partners
-            
+
     def _membership_state(self):
         res = super(res_partner, self)._membership_state()
         today = fields.Date.today()
@@ -170,7 +150,7 @@ class res_partner(models.Model):
                     if (mline.date_to) >= today and (mline.date_from) <= today:
                         if mline.account_invoice_line.invoice_id.partner_id == partner:
                             mstate = mline.account_invoice_line.invoice_id.state
-                            if mstate == 'paid':
+                            if mstate == "paid":
                                 inv = mline.account_invoice_line.invoice_id
                                 if not inv.payment_move_line_ids:
                                     partner.free_member = 1
@@ -178,15 +158,15 @@ class res_partner(models.Model):
                                     partner.free_member = 0
 
             if partner.free_member and s != 0:
-                res[partner.id] = 'free'
-                
+                res[partner.id] = "free"
+
         return res
 
 
 class PartnerImage(models.Model):
-    _name = 'partner.image'
-    _description = 'Partner Image'
+    _name = "partner.image"
+    _description = "Partner Image"
 
-    name = fields.Char('Name')
-    image = fields.Binary('Image', attachment=True)
-    partner_id = fields.Many2one('res.partner', 'Related Partner', copy=True)
+    name = fields.Char("Name")
+    image = fields.Binary("Image", attachment=True)
+    partner_id = fields.Many2one("res.partner", "Related Partner", copy=True)
