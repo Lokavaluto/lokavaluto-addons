70aa98333a2f5658c7da08197ade115369f5ab37
Stéphan Sainléger <contact@stephansainleger.fr>
2021-12-13 18:25:14 +0100

    [UPD] The portal profile forms edit now the partner

--- lcc_members/controllers/portal.py
+++ lcc_members/controllers/portal.py
@@ -1,22 +1,43 @@
 # Copyright 2020 Lokavaluto ()
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import http, _
+import re
+from odoo import http, tools, _
 from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
 from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
 
-from odoo.osv.expression import OR
-
 
 class CustomerPortal(CustomerPortal):
-    def get_domain_my_profiles(self, user):
+    PROFILE_FIELDS = [
+        "function",
+        "phone",
+        "mobile",
+        "email",
+        "website_url",
+        "street",
+        "street2",
+        "city",
+        "country_id",
+        "zipcode",
+        "website_description",
+        "industry_id",
+        "detailed_activity",
+        "reasons_choosing_mlc",
+        "itinerant",
+        "accept_coupons",
+        "accept_digital_currency",
+        "phone_pro",
+        "opening_time",
+        "discount",
+    ]
+
+    def _get_domain_my_profiles(self, user):
         return [("contact_id", "=", user.partner_id.id), ("active", "=", True)]
 
     def _prepare_portal_layout_values(self):
         values = super(CustomerPortal, self)._prepare_portal_layout_values()
         values["profile_count"] = request.env["res.partner"].search_count(
-            self.get_domain_my_profiles(request.env.user)
+            self._get_domain_my_profiles(request.env.user)
         )
         return values
 
@@ -29,6 +50,19 @@ class CustomerPortal(CustomerPortal):
             profile, access_token, values, "my_profiles_history", False, **kwargs
         )
 
+    def _details_profile_form_validate(self, data):
+        error = dict()
+        error_message = []
+
+        # email validation
+        if data.get("email") and not tools.single_email_re.match(data.get("email")):
+            error["email"] = "error"
+            error_message.append(
+                _("Invalid Email! Please enter a valid email address.")
+            )
+
+        return error, error_message
+
     @http.route(
         ["/my/profiles", "/my/profiles/page/<int:page>"],
         type="http",
@@ -40,7 +74,7 @@ class CustomerPortal(CustomerPortal):
     ):
         values = self._prepare_portal_layout_values()
         profile = request.env["res.partner"]
-        domain = self.get_domain_my_profiles(request.env.user)
+        domain = self._get_domain_my_profiles(request.env.user)
 
         searchbar_sortings = {
             "name": {"label": _("Name"), "order": "name"},
@@ -85,26 +119,59 @@ class CustomerPortal(CustomerPortal):
         return request.render("lcc_members.portal_my_profiles", values)
 
     @http.route(
-        ["/my/profile/<int:profile_id>"], type="http", auth="public", website=True
+        ["/my/profile/<int:profile_id>", "/my/profile/save"],
+        type="http",
+        auth="user",
+        website=True,
     )
-    def portal_my_profile(self, profile_id=None, access_token=None, **kw):
+    def portal_my_profile(
+        self, profile_id=None, access_token=None, redirect=None, **kw
+    ):
+        # The following condition is to transform profile_id to an int, as it is sent as a string from the templace "portal_my_profile"
+        # TODO: find a better way to retrieve the profile_id at form submit step
+        if not isinstance(profile_id, int):
+            profile_id = int(profile_id)
+
+        # Check that the user has the right to see this profile
         try:
             profile_sudo = self._document_check_access(
                 "res.partner", profile_id, access_token
             )
         except (AccessError, MissingError):
-            return request.redirect("/my")
+            return request.redirect("/my/profiles")
 
         values = self._profile_get_page_view_values(profile_sudo, access_token, **kw)
-
-        countries = request.env["res.country"].sudo().search([])
-        industries = request.env["res.partner.industry"].sudo().search([])
-
         values.update(
             {
-                "countries": countries,
-                "industries": industries,
+                "error": {},
+                "error_message": [],
             }
         )
 
-        return request.render("lcc_members.portal_my_profile", values)
+        if kw and request.httprequest.method == "POST":
+            # the user has clicked in the Save button to save new data
+            error, error_message = self._details_profile_form_validate(kw)
+            values.update({"error": error, "error_message": error_message})
+            values.update(kw)
+            if not error:
+                values = {key: kw[key] for key in self.PROFILE_FIELDS if key in kw}
+                values.update({"zip": values.pop("zipcode", "")})
+                values.update({"website": values.pop("website_url", "")})
+                profile = request.env["res.partner"].browse(profile_id)
+                profile.sudo().write(values)
+                if redirect:
+                    return request.redirect(redirect)
+                return request.redirect("/my/profiles")
+        else:
+            # This is just the form page opening. We send all the data needed for the form fields
+            countries = request.env["res.country"].sudo().search([])
+            industries = request.env["res.partner.industry"].sudo().search([])
+            values.update(
+                {
+                    "profile_id": profile_id,  # Sent in order to retrieve it at submit time
+                    "countries": countries,
+                    "industries": industries,
+                    "redirect": redirect,
+                }
+            )
+            return request.render("lcc_members.portal_my_profile", values)
