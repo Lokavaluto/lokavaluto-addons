f8e03d3505ed4f6a025ac68f81b16622d355391e
Nicolas JEUDY <nicolas.jeudy@myceliandre.fr>
2022-02-11 12:17:20 +0100

    [FIX] lcc_cyclos_base: improve credit
    - Transfered to principal module
    - Improve paid invoice selector for cyclos

--- lcc_cyclos_base/models/account_invoice.py
+++ lcc_cyclos_base/models/account_invoice.py
@@ -4,24 +4,6 @@ from odoo import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    has_numeric_lcc_products = fields.Boolean(
-        string="Has lcc numeric products",
-        compute="_compute_has_numeric_lcc_products",
-        store=True,
-    )
-
-    @api.one
-    @api.depends("state", "invoice_line_ids.product_id", "invoice_line_ids.price_total")
-    def _compute_has_numeric_lcc_products(self):
-        self.has_numeric_lcc_products = False
-
-        categ = self.env.ref("lcc_cyclos_base.product_category_cyclos")
-        lcc_numeric_products = self.invoice_line_ids.filtered(
-            lambda line: line.product_id.categ_id == categ
-        )
-        if lcc_numeric_products:
-            self.has_numeric_lcc_products = True
-
     cyclos_amount_credited = fields.Float(
         string="Cyclos amount credited", track_visibility="always"
     )
@@ -35,11 +17,12 @@ class AccountInvoice(models.Model):
                 amount = sum(
                     self.invoice_line_ids.filtered(
                         lambda line: line.product_id.categ_id == categ
-                    ).mapped("quantity")
+                    ).mapped("price_subtotal")
                 )
-                invoice.partner_id.action_credit_cyclos_account(amount)
-                invoice.write(
-                    {
-                        "cyclos_amount_credited": amount,
-                    }
-                )
+                if amount > 0:
+                    invoice.partner_id.action_credit_cyclos_account(amount)
+                    invoice.write(
+                        {
+                            "cyclos_amount_credited": amount,
+                        }
+                    )
