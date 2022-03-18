9c48fc11ba70525befb0ecaa3846b1ca52fee1d6
Stéphan Sainléger <contact@stephansainleger.fr>
2022-03-18 23:23:02 +0100

    [UPD] Simplification of invoice portal list
    Replace Due amount by Total amount
    Remove Due Date

--- lcc_members/__manifest__.py
+++ lcc_members/__manifest__.py
@@ -105,6 +105,7 @@ mission is to support the collaborative development of Odoo features and ecosyst
         "views/website_partner_view.xml",
         "views/assets.xml",
         "views/profile_portal_templates.xml",
+        "views/invoice_portal_templates.xml",
         "data/partner_profile_data.xml",
         "data/res_partner_data.xml",
     ],
