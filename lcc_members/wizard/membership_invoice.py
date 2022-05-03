13c128ce771a9ff57f5070ab222ffd9329eeeae9
Stéphan Sainléger <contact@stephansainleger.fr>
2022-05-03 15:04:47 +0200

    [UPD] Make team_id field required only in membership registration
    add a mandatory team_id field in the membership invoice wizard

--- lcc_members/wizard/membership_invoice.py
+++ lcc_members/wizard/membership_invoice.py
@@ -6,11 +6,18 @@ from odoo import api, fields, models
 class MembershipInvoice(models.TransientModel):
     _inherit = "membership.invoice"
 
+    @api.model
+    def _default_team_id(self):
+        return (
+            self.env["res.partner"].browse(self._context.get("active_ids")).team_id.id
+        )
+
     team_id = fields.Many2one(
         "crm.team",
         string="Local group",
         required=True,
         domain=[("local_group", "=", True)],
+        default=_default_team_id,
     )
 
     def membership_invoice(self):
