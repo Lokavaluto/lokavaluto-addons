5f23dbc18e4a27ac1e68986a84f0359b284faf51
Nicolas JEUDY <nicolas.jeudy@myceliandre.fr>
2022-05-09 15:39:28 +0200

    [NEW] add new l10n_fr_lcc_members module (FIXES: #173)

---
+++ l10n_fr_lcc_members/models/account_journal.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+
+    is_receipt = fields.Boolean("Print Receipt")
