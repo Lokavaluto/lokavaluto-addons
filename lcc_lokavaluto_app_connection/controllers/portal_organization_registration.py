fd7c35ee1b002909b93164f2eb05efb05c36d22b
Stéphan Sainléger <contact@stephansainleger.fr>
2022-08-08 16:44:14 +0200

    [IMP] lcc_lokavaluto_app_con..: add wallet creation in orga form
    Adds field so the connected user can refuse the creation of a numeric
    wallet from the organization registration form.
    
    Fixes #242

---
+++ lcc_lokavaluto_app_connection/controllers/portal_organization_registration.py
@@ -0,0 +1,9 @@
+from odoo.addons.lcc_members_portal.controllers.portal_organization_registration import PortalOrganizationRegistration
+
+
+class PortalOrganizationRegistrationWallet(PortalOrganizationRegistration):
+
+    def _compute_organization_form_data(self, data):
+        values = super(PortalOrganizationRegistrationWallet, self)._compute_organization_form_data(data)
+        values["refuse_numeric_wallet_creation"] = data.get("refuse_numeric_wallet_creation", "off") == "on"
+        return values
