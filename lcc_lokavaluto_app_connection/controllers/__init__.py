61f2b89c91f7a26ea55642bfbcecc7cf1c789e53
Stéphan Sainléger <contact@stephansainleger.fr>
2022-08-08 19:10:00 +0200

    [IMP] lcc_lokavaluto_app_con..: add wallet creation in web orga form

--- lcc_lokavaluto_app_connection/controllers/__init__.py
+++ lcc_lokavaluto_app_connection/controllers/__init__.py
@@ -2,3 +2,4 @@ from . import portal
 from . import rest_api
 from . import portal_private_registration
 from . import portal_organization_registration
+from . import website_organization_registration
---
+++ lcc_lokavaluto_app_connection/controllers/website_organization_registration.py
@@ -0,0 +1,9 @@
+from odoo.addons.lcc_members_portal.controllers.website_organization_registration import WebsiteOrganizationRegistration
+
+
+class WebsiteOrganizationRegistrationWallet(WebsiteOrganizationRegistration):
+
+    def _compute_web_form_data(self, data):
+        values = super(WebsiteOrganizationRegistrationWallet, self)._compute_web_form_data(data)
+        values["refuse_numeric_wallet_creation"] = data.get("refuse_numeric_wallet_creation", "off") == "on"
+        return values
