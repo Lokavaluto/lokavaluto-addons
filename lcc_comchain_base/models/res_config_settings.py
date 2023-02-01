a76694dad9676ac9d2cabdd8b91236a01af17ad8
RemiFr82 <remifr82@gmail.com>
2023-02-01 18:58:51 +0100

    [ADD] comchain fields in config settings

--- lcc_comchain_base/models/res_config_settings.py
+++ lcc_comchain_base/models/res_config_settings.py
@@ -8,5 +8,18 @@ class ResConfigSettings(models.TransientModel):
     comchain_currency_name = fields.Char(
         related="company_id.comchain_currency_name",
         readonly=False,
-        string="Currency Name",
+        string="Comchain currency name",
+    )
+
+    config_comchain_delegated_wallet = fields.Char(
+        string='Comchain delegated wallet',
+        config_parameter='comchain.delegated_wallet'
+    )
+    config_comchain_delegated_pwd = fields.Char(
+        string='Comchain delegated password',
+        config_parameter='comchain.delegated_pwd'
+    )
+    config_comchain_stock_id = fields.Char(
+        string='Comchain stock wallet address',
+        config_parameter='comchain.stock_id'
     )
