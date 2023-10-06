3b790c2d78575a781ed5e6616594645f508679b6
Stéphan Sainléger <contact@stephansainleger.fr>
2023-10-06 09:43:44 +0200

    [FIX] lcc_comchain_base: allow _compute_status() on multi records

--- lcc_comchain_base/__manifest__.py
+++ lcc_comchain_base/__manifest__.py
@@ -70,7 +70,7 @@ mission is to support the collaborative development of Odoo features and ecosyst
     "author": "Lokavaluto",
     "website": "https://lokavaluto.fr",
     "category": "Website",
-    "version": "12.0.2.2.0",
+    "version": "12.0.2.2.1",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
