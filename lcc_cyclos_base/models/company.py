4da8818d5ab21519a57bdf033635d18248c718a2
Stéphan Sainléger <contact@stephansainleger.fr>
2023-11-15 00:06:05 +0100

    [IMP] lcc_cyclos_base: add get_cyclos_server_domain function

--- lcc_cyclos_base/models/company.py
+++ lcc_cyclos_base/models/company.py
@@ -10,3 +10,11 @@ class Company(models.Model):
     cyclos_server_login = fields.Char(string="Login for cyclos server")
 
     cyclos_server_password = fields.Char(string="Password for cyclos server")
+
+    def get_cyclos_server_domain(self):
+        self.ensure_one()
+        domain_url = self.cyclos_server_url
+        remove = ["https://", "http://", "/api"]
+        for value in remove:
+            domain_url = domain_url.replace(value, "")
+        return domain_url
