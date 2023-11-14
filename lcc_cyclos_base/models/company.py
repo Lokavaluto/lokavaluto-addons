78c9dbc11f64a8560a082b69d345d2c8f34a1378
Stéphan Sainléger <contact@stephansainleger.fr>
2023-11-15 00:06:05 +0100

    [IMP] lcc_cyclos_base: add get_cyclos_server_domain function

--- lcc_cyclos_base/models/company.py
+++ lcc_cyclos_base/models/company.py
@@ -1,3 +1,7 @@
+import re
+from urllib.parse import urlparse
+from xml.dom import SyntaxErr
+from werkzeug.exceptions import NotFound
 from odoo import models, fields
 
 
@@ -10,3 +14,15 @@ class Company(models.Model):
     cyclos_server_login = fields.Char(string="Login for cyclos server")
 
     cyclos_server_password = fields.Char(string="Password for cyclos server")
+
+    def get_cyclos_server_domain(self):
+        self.ensure_one()
+        url = self.cyclos_server_url
+        if not url:
+            return NotFound("Cyclos URL in Odoo configiration is empty.")
+        parsed_uri = urlparse(url)
+        if not parsed_uri or not parsed_uri.netloc:
+            return SyntaxErr("Cyclos URL in Odoo configiration is not valid.")
+        if not re.search("^[a-z0-9-]+(\.[a-z0-9-]+)*$", parsed_uri.netloc.lower()):
+            raise SyntaxErr("Cyclos URL in Odoo configiration is not valid.")
+        return parsed_uri.netloc
