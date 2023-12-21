c645343bad99df56c40479668cc98027bf8ac35b
Valentin Lab <valentin.lab@kalysto.org>
2023-12-21 18:09:08 +0100

    [UPD] lcc_lokavaluto_app_connection: ensure that partners have public profiles when needed

--- lcc_lokavaluto_app_connection/services/auth_services.py
+++ lcc_lokavaluto_app_connection/services/auth_services.py
@@ -42,9 +42,18 @@ class AuthService(Component):
                     to_add = self._update_auth_data(
                         partner, request.httprequest.authorization.password
                     )
+                    lcc_profile_info = partner.lcc_profile_info()
+                    if len(lcc_profile_info) == 0:
+                        raise exceptions.UserError(
+                            "Invalid User %r (id: %d), related partner %r (id: %d) has no public profile.",
+                            current_user.login,
+                            current_user.id,
+                            partner.name,
+                            partner.id,
+                        )
                     response["prefetch"] = {
                         "backend_credentials": to_add,
-                        "partner": partner.lcc_profile_info()[0],
+                        "partner": lcc_profile_info[0],
                     }
 
                     if to_add:
