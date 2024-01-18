a48b9ffda8bdf2a2469d19fcf836954a5d721c38
Johan Le Baut <johan.lebaut@pm.me>
2024-01-18 17:17:07 +0100

    Removed lcc_lokavaluto_app_connection init by mistake

---
+++ lcc_lokavaluto_app_connection/services/__init__.py
@@ -0,0 +1,8 @@
+from . import ping_services
+from . import partner_images_services
+from . import partner_industry_services
+from . import partner_map_services
+from . import partner_services
+from . import auth_services
+
+__api_version__ = 12
