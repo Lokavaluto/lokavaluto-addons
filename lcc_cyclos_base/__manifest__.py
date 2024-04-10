33bdc6d14ae1da218302dde664102b7585a988c2
Stéphan Sainléger <contact@stephansainleger.fr>
2024-04-10 23:04:41 +0200

    [MIG] lcc_cyclos_base: migrate to 16.0

---
+++ lcc_cyclos_base/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    "name": "lcc_cyclos_base",
+    "summary": """
+        Cyclos models and api for cyclos transaction backend
+        """,
+    "author": "Lokavaluto",
+    "website": "https://lokavaluto.fr",
+    "category": "Website",
+    "version": "16.0.1.0.0",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "base",
+        "lcc_lokavaluto_app_connection",
+        "lcc_members",
+        "auth_oauth",
+    ],
+    # always loaded
+    "data": [
+        "security/ir_rule.xml",
+        "views/res_config_settings_view.xml",
+        "views/res_partner_view.xml",
+        "views/lcc_backend.xml",
+        "data/cyclos_data.xml",
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'lcc_cyclos_base/static/src/scss/signup.scss',
+        ],
+    },
+    # only loaded in demonstration mode
+    "demo": [],
+}
---
+++ lcc_cyclos_base/datamodel/cyclos.py
@@ -0,0 +1,23 @@
+from marshmallow import fields
+
+from odoo.addons.datamodel.core import Datamodel
+from odoo.addons.datamodel.fields import NestedModel
+
+
+class CyclosPartnersInfo(Datamodel):
+    _name = "cyclos.partners.info"
+
+    addresses = fields.List(fields.String(), required=True)
+
+
+class CyclosCreditInfo(Datamodel):
+    _name = "cyclos.credit.info"
+
+    owner_id = fields.Integer(required=True)
+    amount = fields.Float(required=True)
+
+
+class CyclosCreditResponse(Datamodel):
+    _name = "cyclos.credit.response"
+
+    order_url = fields.String(required=True)
