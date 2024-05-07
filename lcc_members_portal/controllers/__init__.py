a2ba9c008d535cc2cf2701c824399a864d6df176
Stéphan Sainléger <contact@stephansainleger.fr>
2024-05-07 11:49:15 +0200

    [MIG] lcc_members_portal : Migration to 16.0

---
+++ lcc_members_portal/controllers/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2020 Lokavaluto ()
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import portal_private_registration
+from . import portal_organization_registration
+from . import website_organization_registration
+from . import portal_organization_renewal
+from . import portal_affiliation_request
