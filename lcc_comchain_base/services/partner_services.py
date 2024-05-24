f020911e1f56fa6eff50992d6a8ebc584b385996
Valentin Lab <valentin.lab@kalysto.org>
2024-05-24 17:07:26 +0200

    [NEW] lcc_comchain_base,lcc_cyclos_base: add safe wallet to backend info

--- lcc_comchain_base/services/partner_services.py
+++ lcc_comchain_base/services/partner_services.py
@@ -11,18 +11,6 @@ _logger = logging.getLogger(__name__)
 class PartnerService(Component):
     _inherit = "partner.service"
 
-    def _update_search_data(self, partner, backend_keys):
-        _logger.debug("SEARCH: backend_keys = %s" % backend_keys)
-        data = super(PartnerService, self)._update_search_data(partner, backend_keys)
-        wallets = partner.get_wallets("comchain")
-        for wallet in wallets:
-            if wallet.comchain_id:
-                for backend_key in backend_keys:
-                    if backend_key.startswith("comchain:"):
-                        data[backend_key] = [wallet.comchain_id]
-        _logger.debug("SEARCH: data %s" % data)
-        return data
-
     def _get_backend_credentials(self, partner):
         data = super(PartnerService, self)._get_backend_credentials(partner)
         wallets = partner.get_wallets("comchain")
