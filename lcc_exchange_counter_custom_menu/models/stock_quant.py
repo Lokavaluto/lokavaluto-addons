8e80c0afe9355a0fb33eb7ecd033c30bc4eff37a
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2024-11-20 09:43:31 +0100

    [ADD]lcc_exchange_counter_custom_menu: add note fields in stock ajustement view

---
+++ lcc_exchange_counter_custom_menu/models/stock_quant.py
@@ -0,0 +1,14 @@
+from odoo import models, fields, api
+
+class StockQuant(models.Model):
+
+    _inherit = "stock.quant"
+
+    note = fields.Text('Note', help="Indiquez ici les modifs de l'ajustement de stock")
+
+    @api.model
+    def _get_inventory_fields_write(self):
+        """ Returns a list of fields user can edit when editing a quant in `inventory_mode`."""
+        res = super()._get_inventory_fields_write()
+        res += ['note']
+        return res
