dff8457f88e7a661b8c5b62acf5655f6da028641
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2025-01-14 18:13:00 +0100

    [ADD]lcc_journal_category

---
+++ lcc_journal_category/models/account_journal.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+
+    category = fields.Selection(
+        [("asso", "compta fonctionnement asso"),
+         ("coupon", "compta coupon/billets MLC"),
+         ("num", "compta MLC numérique")],
+        string="Catégorie",
+        required=True,
+        default="asso",
+    )
