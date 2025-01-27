8414cbb64262d3cbef901f83459c813462cd73d3
Stéphan Sainléger <contact@stephansainleger.fr>
2025-01-27 20:55:24 +0100

    [FIX] lcc_credit_requests_from_contracts: fix credit request data calculation when multiple contracts

--- lcc_credit_requests_from_contracts/models/contract.py
+++ lcc_credit_requests_from_contracts/models/contract.py
@@ -1,4 +1,4 @@
-from odoo import fields, models, api
+from odoo import fields, models
 
 
 class ContractContract(models.Model):
@@ -12,19 +12,18 @@ class ContractContract(models.Model):
     max_credit_amount = fields.Float("Maximum amount of credit allowed")
 
     def _prepare_credit_request_values(self, invoice):
-        self.ensure_one()
-        values = {
-            "amount": self.credit_request_amount
-            if self.different_credit_request_amount
+        contract = invoice.contract_id
+        return {
+            "amount": contract.credit_request_amount
+            if contract.different_credit_request_amount
             else invoice.amount_total,
             "partner_id": invoice.partner_id.id,
             "wallet_id": invoice.contract_id.wallet_id.id,
             "invoice_id": invoice.id,
             "no_order": True,
-            "limit_credit_aggregation": self.limit_credit_aggregation,
-            "max_credit_amount": self.max_credit_amount,
+            "limit_credit_aggregation": contract.limit_credit_aggregation,
+            "max_credit_amount": contract.max_credit_amount,
         }
-        return values
 
     def _prepare_invoice(self, date_invoice, journal=None):
         invoice_vals = super()._prepare_invoice(date_invoice, journal)
