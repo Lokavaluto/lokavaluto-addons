2d09f237c226b3ee8bb23775f2d29eef7d971e96
Valentin Lab <valentin.lab@kalysto.org>
2025-02-04 15:15:18 +0100

    [IMP] lcc_comchain_base: add ``pyc3l`` debug logs

--- lcc_comchain_base/__init__.py
+++ lcc_comchain_base/__init__.py
@@ -2,3 +2,12 @@
 from . import models
 from . import services
 from . import datamodel
+
+import logging
+
+odoo_logger = logging.getLogger('odoo')
+odoo_logger_level = odoo_logger.getEffectiveLevel()
+
+pyc3l_logger = logging.getLogger('pyc3l')
+pyc3l_logger.setLevel(odoo_logger_level)
+
