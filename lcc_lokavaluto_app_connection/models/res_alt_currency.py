3a76bb2930fdf4e3767d36520af128f0468b3006
Stéphan Sainléger <contact@stephansainleger.fr>
2025-02-28 23:54:06 +0100

    [NEW] lcc_lokavaluto_app_connection: create res.alt.currency model
    and associated views

---
+++ lcc_lokavaluto_app_connection/models/res_alt_currency.py
@@ -0,0 +1,40 @@
+from odoo import api, fields, models
+
+
+class AlternativeCurrency(models.Model):
+    """An alternative currency is a currency managed by Odoo on a external financial backend.
+    Each currency can have its own parameters and its wallets.
+    """
+
+    _name = "res.alt.currency"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _description = "Alternative Currency"
+
+    name = fields.Char("Name", tracking=True)
+    active = fields.Boolean(default=True, tracking=True)
+
+    ident = fields.Char("Currency Ident", tracking=True)
+    engine = fields.Selection(
+        [("foo", "Value defined only for testing purposes")],
+        string="Transaction Engine",
+        required=True,
+        tracking=True,
+    )  # You can find real type values in lcc_comchain_base and lcc_cyclos_base,
+    uri = fields.Char("Currency URI", compute="_compute_currency_uri", store=True, tracking=True)
+
+    activate_automatic_topup = fields.Boolean("Activate Automatic Topup", tracking=True)
+    currency_unit_product_id = fields.Many2one(
+        "product.product",
+        string="Currency Unit Product",
+        tracking=True,
+    )
+    commission_product_id = fields.Many2one(
+        "product.product",
+        string="Commission Product",
+        tracking=True,
+    )
+
+    @api.depends("engine", "ident")
+    def _compute_currency_uri(self):
+        for cur in self:
+            cur.uri = "%s://%s" % (cur.engine, cur.ident)
---
+++ lcc_lokavaluto_app_connection/tests/test_res_alt_currency.py
@@ -0,0 +1,23 @@
+from odoo.addons.component.tests.common import TransactionComponentCase
+
+
+class TestResAltCurrency(TransactionComponentCase):
+    def setUp(self):
+        super().setUp()
+
+        self.ResUsers = self.env["res.users"]
+        self.ResPartner = self.env["res.partner"]
+        self.ResPartnerBackend = self.env["res.partner.backend"]
+        self.ResAltCurrency = self.env["res.alt.currency"]
+
+        self.currency_A_unit_product = self.env.ref(
+            "lcc_lokavaluto_app_connection.product_product_numeric_lcc"
+        ).sudo()
+        self.currency_A = self.ResAltCurrency.create(
+            {
+                "name": "Currency A",
+                "active": True,
+                "technology": "foo",
+                "currency_unit_product_id": self.currency_A_unit_product.id,
+            }
+        )
