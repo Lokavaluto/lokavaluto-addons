83c66c9eba12ca3406d3ae2bca121b1406882677
Stéphan Sainléger <contact@stephansainleger.fr>
2025-03-05 15:30:21 +0100

    [IMP] lcc_lokavaluto_app_connection: transfer commission_product_id
    parameter in alt currency model

--- lcc_lokavaluto_app_connection/models/company.py
+++ lcc_lokavaluto_app_connection/models/company.py
@@ -12,9 +12,7 @@ class Company(models.Model):
     monujo_web_app_url = fields.Char(string="Monujo web app URL")
     monujo_android_app_url = fields.Char(string="Monujo Android app URL")
     monujo_ios_app_url = fields.Char(string="Monujo iOS app URL")
-    commission_product_id = fields.Many2one(
-        "product.product", string="Commission Product"
-    )
+
     allow_payments_only_to_companies = fields.Boolean(
         string="Allow payments only to companies"
     )
