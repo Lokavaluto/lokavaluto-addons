13a4a393f0a87786ee082d49313052ad444e1de2
Stéphan Sainléger <contact@stephansainleger.fr>
2025-03-10 22:13:38 +0100

    [IMP] lcc_lokavaluto_app_connection: transfer debit request cron
    from res_company to res_alt_currency models

--- lcc_lokavaluto_app_connection/models/res_alt_currency.py
+++ lcc_lokavaluto_app_connection/models/res_alt_currency.py
@@ -1,4 +1,9 @@
+import logging
+from datetime import datetime
+
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 
 class AlternativeCurrency(models.Model):
@@ -41,3 +46,126 @@ class AlternativeCurrency(models.Model):
 
     def _safe_wallet_partners(self):
         return []
+
+    def _cron_import_new_digital_currency_debit_requests(self) -> None:
+        for alt_currency in self.search([("active", "=", True)]):
+            _logger.info(
+                f"Start sync debit request for alt currency {alt_currency.name}.",
+            )
+            alt_currency._sync_new_debit_requests()
+            _logger.info(
+                f"Sync of debit request for alt currency {alt_currency.name} finished.",
+            )
+
+    def _assert_transaction_valid(self, transaction) -> None:
+        """Check if the transaction has all required fields."""
+        for field in [
+            "sender",
+            "amount",
+            "backend_ident",
+            "transaction_id",
+            "transaction_date",
+        ]:
+            if not transaction.get(field):
+                msg = f"Transaction has no {field}"
+                raise ValueError(msg)
+
+    def _sync_new_debit_requests(self) -> None:
+        """Create the debit requests in Odoo for all the debit
+        transactions performed since the last check.
+        """
+        # For each transaction received for each alternative currency:
+        # - retrieve the transaction id
+        # - check if any debit request exists with this transaction id
+        # - if yes, do nothing
+        # - if no, create a new debit request
+        self.ensure_one()
+        for transaction in self._retrieve_last_debit_transactions():
+            self._assert_transaction_valid(transaction)
+
+            ## replace sender by wallet_id
+
+            wallet_ids = self.env["res.partner.backend"].search(
+                [("name", "=", transaction["sender"])],
+            )
+            if len(wallet_ids) == 0:
+                _logger.warning(
+                    "No wallet found for debit transaction %r (Sender requested: %r)"
+                    % (transaction["transaction_id"], transaction["sender"])
+                )
+                continue
+            if len(wallet_ids) > 1:
+                raise ValueError(
+                    "Too many wallets found for debit transaction %r (Sender requested: %r)"
+                    % (transaction["transaction_id"], transaction["sender"])
+                )
+            transaction["wallet_id"] = wallet_ids[0].id
+            del transaction["sender"]
+
+            ## normalize date to UTC and remove timezone
+
+            transaction["transaction_date"] = datetime.utcfromtimestamp(
+                transaction["transaction_date"].timestamp(),
+            )
+
+            ## check if a debit request already exists for this transaction
+
+            debit_requests = self.env["debit.request"].search(
+                [("transaction_id", "=", transaction["transaction_id"])],
+            )
+            if len(debit_requests) > 0:
+                if len(debit_requests) > 1:
+                    msg = (
+                        "Inconsistency in debit request database, "
+                        "many debit_request exists for transaction id {}".format(
+                            transaction["transaction_id"],
+                        )
+                    )
+                    raise ValueError(
+                        msg,
+                    )
+                ## is this debit request for the same transaction ?
+                for field in ["wallet_id", "amount", "transaction_date"]:
+                    debit_request_value = getattr(debit_requests[0], field)
+                    if field.endswith("_id"):
+                        debit_request_value = debit_request_value.id
+                    if debit_request_value != transaction.get(field):
+                        msg = (
+                            "Debit request already exists with different values for transaction {}"
+                            "(id:{} has different {} value ({}))".format(
+                                transaction["transaction_id"],
+                                debit_requests[0].id,
+                                field,
+                                debit_request_value,
+                            )
+                        )
+                        raise ValueError(
+                            msg,
+                        )
+                _logger.info(
+                    "Debit request already exists for transaction {}, ignoring.".format(
+                        transaction["transaction_id"],
+                    ),
+                )
+                continue
+
+            transaction["active"] = True
+
+            request = self.env["debit.request"].create(transaction)
+
+            _logger.info(
+                "Debit request created for wallet {} on transaction {}".format(
+                    request.wallet_id,
+                    transaction["transaction_id"],
+                ),
+            )
+
+    def _retrieve_last_debit_transactions(self):
+        """TO OVERIDE in digital currency backend dedicated add-ons
+        A list of transactions (dictionary) is expected, with the following data:
+        - sender: the Odoo name of the wallet concerned by the debit request,
+        - amount: the amount debited from the wallet,
+        - tx_id: the transaction ID in the digital currency backend
+        - tx_timestamp: the timestamp of the transaction.
+        """
+        yield from []
