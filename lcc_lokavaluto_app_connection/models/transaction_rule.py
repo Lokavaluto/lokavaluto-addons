f983ab00b53a68f27072977d7bdc93257b6c053a
Quentin Mondot <quent.mondot@hotmail.fr>
2025-04-11 15:19:00 +0200

    [IMP] lcc_lokavaluto_app_connection: created new api route /is_transaction_allowed

--- lcc_lokavaluto_app_connection/models/transaction_rule.py
+++ lcc_lokavaluto_app_connection/models/transaction_rule.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.tools.safe_eval import safe_eval
 
 
 class TransactionRule(models.Model):
@@ -13,5 +14,30 @@ class TransactionRule(models.Model):
     sequence = fields.Integer()
     sender_wallet_domain = fields.Char("Sender Wallet Domain")
     recipient_wallet_domain = fields.Char("Recipient Wallet Domain")
-
     is_transaction_allowed = fields.Boolean("Is Transaction Allowed?")
+
+    recipients_matched_by_rule = fields.Many2many(
+        "res.partner.backend",
+        string="Matched Recipients",
+        compute="_compute_recipients_matched_by_rule",
+        store=False
+    )
+
+    @api.depends("recipient_wallet_domain")
+    def _compute_recipients_matched_by_rule(self):
+        for rule in self:
+            rule.recipients_matched_by_rule = self.env["res.partner.backend"].search(
+                safe_eval(self.recipient_wallet_domain)
+            )
+
+    def recipient_is_allowed_by_rule(self, recipient):
+        if (
+                self.is_transaction_allowed
+                and recipient in self.recipients_matched_by_rule
+        ) or (
+                not self.is_transaction_allowed
+                and recipient not in self.recipients_matched_by_rule
+        ):
+            return True
+        else:
+            return False
