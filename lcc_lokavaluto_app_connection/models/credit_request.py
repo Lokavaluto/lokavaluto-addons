fc0daaa19a1e1206d1921f8316917a50e501dd41
Stéphan Sainléger <contact@stephansainleger.fr>
2025-05-16 11:11:51 +0200

    [IMP] lcc_lokavaluto_app_connection: prevent credit wallet request if not good status
    status must be "pening" or "error"

--- lcc_lokavaluto_app_connection/models/credit_request.py
+++ lcc_lokavaluto_app_connection/models/credit_request.py
@@ -175,6 +175,9 @@ class CreditRequest(models.Model):
     def credit_wallet(self):
         """Send credit order to the wallet."""
         for record in self:
+            if not record.state in ["pending", "error"]:
+                continue
+
             # Check if we have the needed data to perform the top up process
             amount_data = record.compute_amount_to_credit()
 
