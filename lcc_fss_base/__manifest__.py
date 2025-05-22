4aa9461fe41e49c21f06813cdde04d33eaea506c
Quentin Mondot <quentin.mondot@elabore.coop>
2025-05-22 17:25:13 +0200

    [IMP] lcc_fss_base: new fields in res_partner model and update personal_information_page in contact form

--- lcc_fss_base/__manifest__.py
+++ lcc_fss_base/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "lcc_fss_base",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.2",
     "author": "Elabore",
     "website": "https://elabore.coop",
     "maintainer": "Boris Gallet",
@@ -26,8 +26,10 @@
     },
     # always loaded
     "data": [
+        "security/ir.model.access.csv",
         "views/res_partner_view.xml",
         "views/portal_my_home.xml",
+        "data/mandatory_modifier_data.xml"
     ],
     # only loaded in demonstration mode
     "demo": [],
