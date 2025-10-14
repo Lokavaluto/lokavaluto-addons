520f49cacffd8bd12ec801a0f4c18d84771ef11f
Stéphan Sainléger <contact@stephansainleger.fr>
2025-10-15 00:12:09 +0200

    [IMP] lcc_lokavaluto_app_connection: concatenate same currency wallets in credential_backends service answer

--- lcc_lokavaluto_app_connection/tests/__init__.py
+++ lcc_lokavaluto_app_connection/tests/__init__.py
@@ -2,4 +2,5 @@ from . import test_partner_services_wallet_restriction_rules
 from . import test_partner_services_credit_requests
 from . import test_partner_services_reconversions
 from . import test_partner_services_search
+from . import test_partner_services_backend_credentials
 from . import test_res_alt_currency
