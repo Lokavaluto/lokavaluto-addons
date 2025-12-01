5e7020f9ec5491003bceb84d4f374017b076db3f
Stéphan Sainléger <contact@stephansainleger.fr>
2025-12-01 22:30:45 +0100

    [IMP] lcc_comchain_base: move Comchain transaction content checks in a dedicated function

--- lcc_comchain_base/tests/test_wallet.py
+++ lcc_comchain_base/tests/test_wallet.py
@@ -130,3 +130,64 @@ class TestResWallet(TransactionComponentCase):
             "LoremIpsum"
         )
         self.assertFalse(res)
+
+    def test_check_transaction_content_ok(self):
+        """ Test check_transaction_content() for a valid transaction. """
+
+        # Create data
+        currency = self._create_alt_currency()
+        partner = self._create_res_partner()
+        wallet = self._create_res_partner_backend(partner, currency)
+
+        # Mock a transaction response content
+        mock_tx = Mock("transaction", data={"recieved": 1000})
+        mock("Pyc3l.Transaction", returns=mock_tx)
+
+        # Check the transaction
+        response = "0x85f8dfd7e5eab0fe66145ffb0ef3435c75875943b4b589bb0ee6042b4efb1e2e"
+        res = wallet.check_transaction_content(response, 10.00)
+        self.assertFalse(res)
+        restore()
+
+    def test_check_transaction_content_ko_wrong_amount(self):
+        """ Test check_transaction_content() for a transaction with wrong amount. """
+
+        # Create data
+        currency = self._create_alt_currency()
+        partner = self._create_res_partner()
+        wallet = self._create_res_partner_backend(partner, currency)
+
+        # Mock a transaction response content
+        mock_tx = Mock("transaction", data={"recieved": 10})
+        mock("Pyc3l.Transaction", returns=mock_tx)
+
+        # Check the transaction
+        response = "0x85f8dfd7e5eab0fe66145ffb0ef3435c75875943b4b589bb0ee6042b4efb1e2e"
+        res = wallet.check_transaction_content(response, 10.00)
+        self.assertEqual(
+            res,
+            "Order sent, but checking transaction record returned as an unexepected "
+            "amount of 10 received."
+        )
+        restore()
+
+    def test_check_transaction_content_ko_missing_recieved(self):
+        """ Test check_transaction_content() for a transaction with missing field. """
+
+        # Create data
+        currency = self._create_alt_currency()
+        partner = self._create_res_partner()
+        wallet = self._create_res_partner_backend(partner, currency)
+
+        # Mock a transaction response content
+        mock_tx = Mock("transaction", data={"value": 10})
+        mock("Pyc3l.Transaction", returns=mock_tx)
+
+        # Check the transaction
+        response = "0x85f8dfd7e5eab0fe66145ffb0ef3435c75875943b4b589bb0ee6042b4efb1e2e"
+        res = wallet.check_transaction_content(response, 10.00)
+        self.assertEqual(
+            res,
+            "Max retry reached to get transaction info (10 retries)"
+        )
+        restore()
