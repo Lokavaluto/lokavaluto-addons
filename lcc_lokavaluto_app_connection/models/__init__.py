9afd6a4ba0992ee1e57b7a1058fc963159eb0f85
Stéphan Sainléger <contact@stephansainleger.fr>
2025-12-26 16:05:10 +0100

    [NEW] lcc_lokavaluto_app_connection: add payment request model, views and menu

--- lcc_lokavaluto_app_connection/models/__init__.py
+++ lcc_lokavaluto_app_connection/models/__init__.py
@@ -14,3 +14,4 @@ from . import wallet_restriction_rule
 from . import lokavaluto_private_services_collection
 from . import res_alt_currency
 from . import unusual_transaction
+from . import payment_request
