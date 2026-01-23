0db85645f8ce3269340fa6d263d0f88615760e36
Stéphan Sainléger <contact@stephansainleger.fr>
2026-01-23 15:03:03 +0100

    [IMP] lcc_lokavaluto_app_connection: add function to transform wallet old format to wallet uris

--- lcc_lokavaluto_app_connection/tests/test_tools.py
+++ lcc_lokavaluto_app_connection/tests/test_tools.py
@@ -1,5 +1,5 @@
 from odoo.addons.component.tests.common import TransactionComponentCase
-from ..tools import transform_backend_keys_in_currency_uris
+from ..tools import transform_backend_keys_in_currency_uris,transform_wallet_backend_keys_in_wallet_uris
 
 
 class TestResWallet(TransactionComponentCase):
@@ -19,3 +19,20 @@ class TestResWallet(TransactionComponentCase):
         ]
         result = transform_backend_keys_in_currency_uris(backend_keys)
         self.assertEqual(result, expected_uris)
+
+    def test_transform_wallet_backend_keys_in_wallet_uris(self):
+        """Test the transformation of wallet backend keys into wallet URIs.
+
+        Both old and new formats should be correctly transformed to the new URI format.
+        """
+        wallet_backend_keys = [
+            "foo:walletA", # Old format
+            "foo://currencyA/wallet/walletB", # New format
+        ]
+        currency_ident = "currencyA"
+        expected_wallet_uris = [
+            "foo://currencyA/wallet/walletA",
+            "foo://currencyA/wallet/walletB",
+        ]
+        result = transform_wallet_backend_keys_in_wallet_uris(wallet_backend_keys, currency_ident)
+        self.assertEqual(result, expected_wallet_uris)
