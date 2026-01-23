50c8b5af276e234519838e9c89c9d0b5c7c2115c
Stéphan Sainléger <contact@stephansainleger.fr>
2026-01-23 14:30:19 +0100

    [IMP] lcc_lokavaluto_app_connection: move transform_backend_keys_in_currency_uris in tools.py

--- lcc_lokavaluto_app_connection/services/partner_services.py
+++ lcc_lokavaluto_app_connection/services/partner_services.py
@@ -6,7 +6,7 @@ from odoo.exceptions import (
     MissingError,
 )
 from odoo.http import request
-
+from ..tools import transform_backend_keys_in_currency_uris
 from odoo.addons.base_rest import restapi
 from odoo.addons.base_rest.components.service import to_bool, to_int
 from odoo.addons.base_rest_datamodel.restapi import Datamodel
@@ -82,7 +82,7 @@ class PartnerService(Component):
         backend_keys = set(self.env.user.partner_id.backends()) & set(
             partner_credit_requests_get_param.backend_keys,
         )
-        currency_uris = self._transform_backend_keys_in_currency_uris(backend_keys)
+        currency_uris = transform_backend_keys_in_currency_uris(backend_keys)
 
         # Retrieve all the credit requests of the requested currencies
         credit_requests = self.env["credit.request"].sudo().search(
@@ -126,7 +126,7 @@ class PartnerService(Component):
         ## Break of workaround
 
         backend_keys = set(self.env.user.partner_id.backends()) & set(backend_keys)
-        currency_uris = self._transform_backend_keys_in_currency_uris(backend_keys)
+        currency_uris = transform_backend_keys_in_currency_uris(backend_keys)
 
         ## Continue the workaround
         currency_uris += [
@@ -290,7 +290,7 @@ class PartnerService(Component):
 
         # Transform backend_keys in backend_URI if needed
         # TO BE REMOVED once Monujo sends URIs through the API
-        currency_uris = self._transform_backend_keys_in_currency_uris(backend_keys)
+        currency_uris = transform_backend_keys_in_currency_uris(backend_keys)
         alt_currency_ids = self.env["res.alt.currency"].search(
             [("uri", "in", currency_uris)],
             #limit=1,
@@ -433,7 +433,7 @@ class PartnerService(Component):
         backend_keys = set(self.env.user.partner_id.backends()) & set(
             request.params["backend_keys"],
         )
-        currency_uris = self._transform_backend_keys_in_currency_uris(backend_keys)
+        currency_uris = transform_backend_keys_in_currency_uris(backend_keys)
         ## XXXvlab: temporary fix to work with cyclos
         if "@" in request.params["data"]["rpb"]:
             request.params["data"]["rpb"] = request.params["data"]["rpb"].split("@", 1)[
@@ -489,7 +489,7 @@ class PartnerService(Component):
         backend_keys = self.env.user.partner_id.backends() & set(
             account_search_info.backend_keys,
         )
-        currency_uris = self._transform_backend_keys_in_currency_uris(backend_keys)
+        currency_uris = transform_backend_keys_in_currency_uris(backend_keys)
         recipients = self.env["res.partner.backend"].search(
             [("status", "=", "to_confirm"), ("alt_currency_id.uri", "in", currency_uris)],
         )
@@ -589,25 +589,6 @@ class PartnerService(Component):
 
     def _get(self, _id):
         return self.env["res.partner"].sudo().browse(_id)
-
-    def _transform_backend_keys_in_currency_uris(self, backend_keys):
-        '''
-        Transition function to transform backend keys format in backend URI format.
-        TO BE REMOVED once Monujo uses backend URIs
-        '''
-        currency_uris = []
-        for backend in backend_keys:
-            separator_count = backend.count("://")
-            if  separator_count == 1:
-                # backend matches wished URI structure
-                currency_uris.append(backend)
-            elif separator_count == 0:
-                # backend is OLD format
-                engine,ident = backend.split(":", 1)
-                currency_uris.append(f"{engine}://{ident}")
-            else:
-                raise MissingError(f"Invalid backend id {backend}")
-        return currency_uris
 
     def _get_formatted_recipients(self, recipients, currency_uris):
         rows = []
--- lcc_lokavaluto_app_connection/tests/__init__.py
+++ lcc_lokavaluto_app_connection/tests/__init__.py
@@ -5,3 +5,4 @@ from . import test_partner_services_search
 from . import test_partner_services_backend_credentials
 from . import test_res_alt_currency
 from . import test_partner_services_report_contact_info
+from . import test_tools
