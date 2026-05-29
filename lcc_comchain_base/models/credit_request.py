3f2431b64e60e592317b42b8df34b4e83359bf15
Stéphan Sainléger <contact@stephansainleger.fr>
2026-05-29 14:38:52 +0200

    [IMP] lcc_comchain_base: add automation on credit request attempts

--- lcc_comchain_base/models/credit_request.py
+++ lcc_comchain_base/models/credit_request.py
@@ -4,6 +4,8 @@ from odoo import models
 
 _logger = logging.getLogger(__name__)
 
+RPLCMNT_TX_UNDERPRICED = "Failed transfer on behalf transaction: API Call failed with message: replacement transaction underpriced"
+
 class CreditRequest(models.Model):
 
     _inherit = "credit.request"
@@ -28,3 +30,25 @@ class CreditRequest(models.Model):
 
         # If we reach this point, the transaction is now valid
         self.state = "done"
+
+    def _new_credit_attempt_allowed(self):
+        """Define if a new credit attempt is allowed considering the error message"""
+        self.ensure_one()
+        if self.state != "error" or not self.error_message:
+            return False
+
+        # Replacement transaction underpriced: we know for sure that the
+        # previous transaction attempt is lost.
+        if self.error_message == RPLCMNT_TX_UNDERPRICED:
+            return True
+
+        return False
+
+    def renew_credit_attempt(self):
+        """Launch a new credit request to Comchain if known error message."""
+        self.ensure_one()
+        if self.alt_currency_id.engine != "comchain":
+            return
+
+        if self._new_credit_attempt_allowed():
+            self.credit_wallet()
